c81c8eb87f06c67f8114b9ad2082e1eab21027cc
Masai Wallace <mooseye2010@gmail.com>
2025-12-18 17:27:58 +0000

    Try fixing this error: `Runtime ReferenceError: Button is not defined. E

--- src/components/dashboard/student/student-dashboard.tsx
+++ src/components/dashboard/student/student-dashboard.tsx
@@ -11,6 +11,7 @@ import { getStudentTripProgress } from '@/lib/utils';
 import { format } from 'date-fns';
 import { Progress } from '@/components/ui/progress';
 import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 export function StudentDashboard() {
   const { user } = useAuth();
