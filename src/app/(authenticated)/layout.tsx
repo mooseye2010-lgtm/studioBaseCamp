27f1c884ea72eea99a79bc3f050f4fcc780d0693
Masai Wallace <mooseye2010@gmail.com>
2025-12-18 17:08:32 +0000

    Scrolling features arent working

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -27,7 +27,7 @@ export default function AuthenticatedLayout({
   return (
     <div className="flex h-svh flex-col">
       <AppHeader />
-      <main className="flex-1 flex flex-col overflow-hidden">{children}</main>
+      <main className="flex-1 flex flex-col">{children}</main>
     </div>
   );
 }
